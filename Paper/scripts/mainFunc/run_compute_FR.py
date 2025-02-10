975c3e8b1cdf504a20fddaf5d38ffffbb8c9e940
Nilanjana <nilanjana@eduroam-1-231-45.univ-amu.fr>
2025-02-10 12:34:41 +0100

    Made changes in the main FR function to compute both monkeys together

--- Paper/scripts/mainFunc/run_compute_FR.py
+++ Paper/scripts/mainFunc/run_compute_FR.py
@@ -37,10 +37,11 @@ For aligned data, first FR is computed and then the mean of that is returned as 
 
 def run_compute_FR(inFolder,outFolder,alignData,eventMarker):
     
-    #modules import
-    
-    import sys, pickle, os
-    sys.path.append('/Users/nilanjana/Documents/LabWorkBench/IntrinsicTimescales/scripts/Processing_Methods')
+    if server == '/envau':
+        sys.path.append('/envau/work/comco/nandi.n/IntrinsicTimescales/Paper/scripts/procMethods')
+    else:
+        sys.path.append('/Users/nilanjana/Documents/LabWorkBench/IntrinsicTimescales/Paper/scripts/procMethods')
+        
     from compute_FR import compute_firingRate
     from open_matlabData import open_matlab_behaviour
  
@@ -48,7 +49,6 @@ def run_compute_FR(inFolder,outFolder,alignData,eventMarker):
     # Create folder if it does not exist
     if not os.path.exists(outFolder):
         os.makedirs(outFolder)
-
 
      #import data
      # Search for CSV files in the folder
@@ -89,9 +89,6 @@ def run_compute_FR(inFolder,outFolder,alignData,eventMarker):
          elif eventMarker == 'full':
              t_st= data[l[1]]
              t_end= data[l[2]]
-            
-            
-         
          
          # Extract the last string name from the file name
          last_name = os.path.splitext(os.path.basename(file_name))[0]
@@ -119,9 +116,10 @@ def run_compute_FR(inFolder,outFolder,alignData,eventMarker):
                  
 if __name__=='__main__':
    #monkey = 'Tomy'
-    monkey = 'Mourad'
-
-    import os 
+    imonkey = ['Mourad', 'Tomy']
+    
+    import sys,re, pickle, os 
+    
     current_path = os.getcwd()
     if current_path.startswith('/Users'):
         server = '/Volumes'  # local w VPN
@@ -129,32 +127,34 @@ if __name__=='__main__':
         server = '/envau'  # niolon
     elif current_path.startswith('/envau'):
         server = '/envau'  # niolon
-       
-    if monkey == 'Tomy':
-        
     
-        session_names =['t140924003','t140924006','t140925001','t140926002','t140930001','t141001001','t141008001','t141010003','t141119001','t150122001',
-                        't150123001','t150128001','t150129002','t150204001','t150205004','t150210001','t150211003','t150212001','t150218001','t150219003',
-                        't150303002','t150319003','t150324002','t150327002','t150327003','t150415002','t150416002','t150423002','t150430002','t150520003',
-                        't150716001']
-    if monkey == 'Mourad':
+    for monkey in imonkey:
         
-        session_names= ['Mo180328001','Mo180330001','Mo180405001','Mo180405004','Mo180411001','Mo180412002',
-                        'Mo180411001','Mo180418002','Mo180419003','Mo180503002', 'Mo180523002','Mo180524003', 
-                        'Mo180525003','Mo180530004','Mo180531002','Mo180601001','Mo180614002','Mo180614006',
-                        'Mo180615002','Mo180615005','Mo180426004', 'Mo180427003', 'Mo180619002','Mo180622002',
-                        'Mo180626003', 'Mo180627003','Mo180629005', 'Mo180703003','Mo180704003', 'Mo180705002',
-                         'Mo180706002', 'Mo180710002','Mo180711004','Mo180712006']
+        if monkey == 'Tomy':
+            
+        
+            session_names =['t140924003','t140924006','t140925001','t140926002','t140930001','t141001001','t141008001','t141010003','t141119001','t150122001',
+                            't150123001','t150128001','t150129002','t150204001','t150205004','t150210001','t150211003','t150212001','t150218001','t150219003',
+                            't150303002','t150319003','t150324002','t150327002','t150327003','t150415002','t150416002','t150423002','t150430002','t150520003',
+                            't150716001','TomyAlmap2023']
+        if monkey == 'Mourad':
+            
+            session_names= ['Mo180328001','Mo180330001','Mo180405001','Mo180405004','Mo180411001','Mo180412002',
+                            'Mo180411001','Mo180418002','Mo180419003','Mo180503002', 'Mo180523002','Mo180524003', 
+                            'Mo180525003','Mo180530004','Mo180531002','Mo180601001','Mo180614002','Mo180614006',
+                            'Mo180615002','Mo180615005','Mo180426004', 'Mo180427003', 'Mo180619002','Mo180622002',
+                            'Mo180626003', 'Mo180627003','Mo180629005', 'Mo180703003','Mo180704003', 'Mo180705002',
+                             'Mo180706002', 'Mo180710002','Mo180711004','Mo180712006']
+               
+        alignData = 0
+        evtMarker = 'full'
+        for i_session,sess in enumerate(session_names):
+            
+           print(f"Procession Session{sess}")
+           inFolder = f"{server}/work/comco/nandi.n/IntrinsicTimescales/data/{monkey}/{sess}/modifiedData" 
+           outFolder = f"{server}/work/comco/nandi.n/IntrinsicTimescales/Paper/data/{monkey}/SUA/{sess}/FR_align{alignData}" 
+           run_compute_FR(inFolder, outFolder,alignData, None)
            
-    alignData = 0
-    evtMarker = 'full'
-    for i_session,sess in enumerate(session_names):
-        
-       print(f"Procession Session{sess}")
-       inFolder = f"{server}/work/comco/nandi.n/IntrinsicTimescales/data/{monkey}/{sess}/modifiedData" 
-       outFolder = f"{server}/work/comco/nandi.n/IntrinsicTimescales/data/{monkey}/{sess}/FR" 
-       run_compute_FR(inFolder, outFolder,alignData, None)
-       
-    print('Success computing FR for all sessions')
-         
+        print('Success computing FR for all sessions')
+             
  
